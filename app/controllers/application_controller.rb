ae7f0d1ab145a70cadcfbfcdb10e11abe93bfaae
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-02-26 15:29:03 +0300

    adds routes for the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,89 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # create an owner account
+  post "/owners" do
+    Owner.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      location: params[:location],
+      avatar: params[:avatar],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    ).to_json
+    
+  end
+  # create an rider account
+  post "/riders" do
+    Rider.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      location: params[:location],
+      avatar: params[:avatar],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      id_number: params[:id_number],
+      license_number: params[:license_number],
+      spouse_contact: params[:spouse_contact],
+    ).to_json
+    
+  end
+  # create an bike 
+  post "/bikes" do
+    Bike.create(
+      model: params[:model],
+      reg_number: params[:reg_number],
+      cc: params[:cc],
+      price: params[:price],
+      booked: params[:booked],
+      owner_id: params[:owner_id]
+    ).to_json
+    
+  end
+  # get all bikers on platfrom
+  get "/riders" do
+    riders = Rider.all
+    riders.to_json
+  end
+  # see all my bikes as an owner
+  get "/bikes/:owner_id" do    
+    myBikes = Bike.where(owner_id: params[:owner_id])
+    myBikes.to_json
+  end
+  # TODO: show my paid for bikes
+  get "/bikes/:owner_id/payments" do
+    mybikes = Bike.where(owner_id: params[:owner_id])
+    mybikes.to_json
+    # totalAmount = mybikes.price.to_i
+  end
+  # see all bikes on platform
+  get "/bikes" do
+    Bike.all.to_json    
+  end
+  # see all my hired bikes
+  get "/bikes/:rider_id" do
+    myBikes = Bike.where(rider_id: params[:rider_id])
+    myBikes.to_json
+  end
+  # hire bike 
+  patch '/bikes/:id' do
+    bike = Bike.find(params[:id])
+    bike.update(
+      booked: params[:booked],
+      rider_id: params[:rider_id]
+    )
+    bike.to_json
   end
 
+  # delete a rider acc
+  delete "/riders/:id" do
+    rider = Rider.find(params[:id])
+    rider.destroy.to_json
+  end
+
+  # delete an owner acc
+  delete "/owners/:id" do
+    ownerToDelete = Owner.find(params[:id])
+    ownerToDelete.destroy.to_json    
+  end
 end
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,4 +1,4 @@
 class Owner < ActiveRecord::Base
     has_many :bikes
-    has_many :riders, through: :bikes    
+    has_many :riders, through: :bikes  
 end
